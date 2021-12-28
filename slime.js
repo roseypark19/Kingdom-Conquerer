0212608790b1922be78d402475d358bc371909d1
Parker Rosengreen <parkerrosengreen19@gmail.com>
2021-12-27 18:14:44 -0800

    small slime death bug fixed

--- slime.js
+++ slime.js
@@ -50,7 +50,7 @@ class BabySlime {
 
         if (this.state !== 3) {
             this.game.entities.forEach(entity => {
-                if (entity.friendlyProjectile === true && this.hitBB.collide(entity.hitBB) && !(this.shotsTaken.includes(entity.id)) && this.state !== 4) {
+                if (entity.friendlyProjectile === true && this.hitBB.collide(entity.hitBB) && !(this.shotsTaken.includes(entity.id)) && this.state !== 3) {
                     this.shotsTaken.push(entity.id);
                     this.damagedTimer = 0.6 - this.game.clockTick;
                     this.state = 2;
@@ -202,7 +202,7 @@ class MotherSlime {
 
         if (this.state !== 3) {
             this.game.entities.forEach(entity => {
-                if (entity.friendlyProjectile === true && this.hitBB.collide(entity.hitBB) && !(this.shotsTaken.includes(entity.id)) && this.state !== 4) {
+                if (entity.friendlyProjectile === true && this.hitBB.collide(entity.hitBB) && !(this.shotsTaken.includes(entity.id)) && this.state !== 3) {
                     this.shotsTaken.push(entity.id);
                     if (this.damagedTimer === 0 && this.deadTimer === 0) {
                         this.damagedTimer = 0.6 - this.game.clockTick;
