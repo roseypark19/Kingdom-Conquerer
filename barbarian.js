f98db0abbdfc2759a858aaae76c7c4e358a76f14
Parker Rosengreen <parkerrosengreen19@gmail.com>
2021-12-29 13:52:42 -0800

    fixed barbarian head cutoff

--- barbarian.js
+++ barbarian.js
@@ -222,7 +222,7 @@ class Barbarian {
     updateBB() {
         this.BB = new BoundingBox(this.x, this.y, 32 * PARAMS.SCALE, 32 * PARAMS.SCALE);
         this.hitBB = new BoundingBox(this.x + 12 * PARAMS.SCALE, this.y + 12 * PARAMS.SCALE, 8 * PARAMS.SCALE, 8 * PARAMS.SCALE);
-        this.collisionBB = new BoundingBox(this.hitBB.x, this.hitBB.y + 4 * PARAMS.SCALE, 8 * PARAMS.SCALE, 4 * PARAMS.SCALE);
+        this.collisionBB = new BoundingBox(this.hitBB.x, this.hitBB.y + 2 * PARAMS.SCALE, 8 * PARAMS.SCALE, 6 * PARAMS.SCALE);
     };
     
     draw(ctx) {
